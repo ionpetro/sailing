b804368abf4bacb8ae410f9e7968384f421398cd
Ion Petro <ionpetro@gmail.com>
2025-01-15 10:40:47 +0200

    update video

--- components/hero.tsx
+++ components/hero.tsx
@@ -67,7 +67,7 @@ export default function Hero() {
             {/* Video */}
             <div className="relative aspect-video mb-8">
               <iframe
-                src="https://player.vimeo.com/video/1046446082?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                src="https://player.vimeo.com/video/1047043063?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;quality=2k"
                 className="absolute inset-0 h-full w-full rounded-lg outline-none"
                 width="1920"
                 height="1080"
